3b02cc3fa30b3c6f4f483521ee5506138cc5737a
Lenyn Alcantara <nightzpy@gmail.com>
2017-10-20 15:14:54 -0400

    add test for check valid cube from controller

---
+++ tests/Feature/ExecuteCubeOperationsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ExecuteCubeOperationsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function test_a_user_can_make_a_valid_cube()
+    {
+        $n = 5;
+        $cube = [];
+        for ($x = 0; $x < $n; $x ++) {
+            $cube[$x] = [];
+            for ($y = 0; $y < $n; $y ++) {
+                $cube[$x][$y] = [];
+                for ($z = 0; $z < $n; $z ++)
+                    $cube[$x][$y][$z] = 0;
+            }
+        }
+
+
+        $this->get(route('cube.store'))
+             ->assertJson($cube);
+    }
+}
